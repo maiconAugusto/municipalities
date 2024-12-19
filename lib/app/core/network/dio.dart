b7d957156ebef4a90afa94313bfb731fa4c8edb3
Maicon augusto <maiconaugustocarvalho@gmail.com>
2024-12-19 12:24:13 -0400

    initialize Dio config

---
+++ lib/app/core/network/dio.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+
+class DioClient {
+  final Dio _dio;
+
+  DioClient(String baseUrl)
+      : _dio = Dio(
+          BaseOptions(
+            baseUrl: baseUrl,
+            connectTimeout: const Duration(seconds: 5),
+            receiveTimeout: const Duration(seconds: 3),
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+            },
+          ),
+        ) {
+    _dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestBody: true,
+      responseBody: true,
+      error: true,
+    ));
+  }
+
+  Dio get client => _dio;
+}
