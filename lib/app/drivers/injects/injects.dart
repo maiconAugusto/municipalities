5c34d36df62c7285618ebddbefceb6546f546bf0
Maicon augusto <maiconaugustocarvalho@gmail.com>
2024-12-20 07:25:48 -0400

    read env on getIt

--- lib/app/drivers/injects/injects.dart
+++ lib/app/drivers/injects/injects.dart
@@ -1,9 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:municipalities/app/core/network/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void configureDependencies() {
   final injector = GetIt.instance;
   injector.registerLazySingleton(
-    () => DioClient('').client,
+    () => DioClient(dotenv.env['BASE_URL'] as String).client,
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:municipalities/app/drivers/injects/injects.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
+  configureDependencies();
   runApp(const MyApp());
 }
 
