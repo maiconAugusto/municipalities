0b179b6e799f45887907e37d63c817bb7db57e0e
Maicon augusto <maiconaugustocarvalho@gmail.com>
2024-12-20 07:48:39 -0400

    add city entity

---
+++ lib/app/domain/entities/city_entity.dart
@@ -0,0 +1,11 @@
+class CityEntity {
+  final String name;
+  final String state;
+  final String stateAcronym;
+
+  CityEntity({
+    required this.name,
+    required this.state,
+    required this.stateAcronym,
+  });
+}
